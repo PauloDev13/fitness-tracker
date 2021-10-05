f0656e971a2e5bc2ee92c3ba75bc0adf32bf4410
PauloDev13 <devpgm2020@gmail.com>
2021-10-05 01:00:15 -0300

    Adding ESLint configuration

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,16 +1,15 @@
-import { TestBed } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import { TestBed } from "@angular/core/testing";
 
-describe('AppComponent', () => {
+import { AppComponent } from "./app.component";
+
+describe("AppComponent", () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
-  it('should create the app', () => {
+  it("should create the app", () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
@@ -19,13 +18,15 @@ describe('AppComponent', () => {
   it(`should have as title 'fitness-tracker'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('fitness-tracker');
+    expect(app.title).toEqual("fitness-tracker");
   });
 
-  it('should render title', () => {
+  it("should render title", () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('fitness-tracker app is running!');
+    expect(compiled.querySelector(".content span")?.textContent).toContain(
+      "fitness-tracker app is running!"
+    );
   });
 });
