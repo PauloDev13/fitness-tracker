9815f00b7249adbe6c65d252932e0403b91e43a2
PauloDev13 <devpgm2020@gmail.com>
2021-10-06 15:29:46 -0300

    Adding Datepicker Component to SignUp Form

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MAT_DATE_LOCALE, MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
@@ -7,6 +9,19 @@ import { MatInputModule } from '@angular/material/input';
 @NgModule({
   declarations: [],
   imports: [],
-  exports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule],
+  exports: [
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+  ],
+  providers: [
+    {
+      provide: MAT_DATE_LOCALE,
+      useValue: 'pt-BR',
+    },
+  ],
 })
 export class MaterialModule {}
