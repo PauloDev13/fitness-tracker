592f41f1a56d198af50c79a9ec519106c74d11e0
PauloDev13 <devpgm2020@gmail.com>
2021-10-06 22:22:39 -0300

    Refactoring Toolbar and Sidenav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { MaterialModule } from './material.module';
+import { HeaderComponent } from './navigation/header/header.component';
+import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component';
 import { CurrentTrainingComponent } from './training/current-training/current-training.component';
 import { NewTrainingComponent } from './training/new-training/new-training.component';
 import { PastTrainingComponent } from './training/past-training/past-training.component';
@@ -25,6 +27,8 @@ import { WelcomeComponent } from './welcome/welcome.component';
     PastTrainingComponent,
     WelcomeComponent,
     SignupComponent,
+    HeaderComponent,
+    SidenavListComponent,
   ],
   imports: [
     BrowserModule,
