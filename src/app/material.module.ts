1b250024ae372ba3ddc97294491c4085bfef269a
PauloDev13 <devpgm2020@gmail.com>
2021-10-06 23:39:04 -0300

    Adding a Dropdown Menu

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -8,6 +8,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
+import { MatSelectModule } from '@angular/material/select';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -28,6 +29,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     MatListModule,
     MatTabsModule,
     MatCardModule,
+    MatSelectModule,
   ],
   providers: [
     {
