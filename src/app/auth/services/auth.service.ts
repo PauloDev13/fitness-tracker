7375c9036773ca631040b32905220935fc20c43e
PauloDev13 <devpgm2020@gmail.com>
2021-10-10 22:19:22 -0300

    Adding User Login

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -4,22 +4,20 @@ import { Router } from '@angular/router';
 import { Subject } from 'rxjs';
 
 import { AuthData } from '../models/auth-data-model';
-import { User } from '../models/user-model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   public authChange = new Subject<boolean>();
-  private user!: User;
+  private isAuthenticated = false;
 
   constructor(private router: Router, private FirebaseAuth: AngularFireAuth) {}
 
   registerUser(authData: AuthData) {
     const { email, password } = authData;
     this.FirebaseAuth.createUserWithEmailAndPassword(email, password)
-      .then((result) => {
-        console.log(result);
+      .then(() => {
         this.authSuccessfully();
       })
       .catch((error) => console.log(error));
@@ -33,8 +31,7 @@ export class AuthService {
   login(authData: AuthData) {
     const { email, password } = authData;
     this.FirebaseAuth.signInWithEmailAndPassword(email, password)
-      .then((result) => {
-        console.log(result);
+      .then(() => {
         this.authSuccessfully();
       })
       .catch((error) => console.log(error));
@@ -47,20 +44,17 @@ export class AuthService {
   }
 
   logout() {
-    this.user = { email: '', password: '' };
     this.authChange.next(false);
     this.router.navigate(['/login']).then();
-  }
-
-  getUser(): User {
-    return { ...this.user };
+    this.isAuthenticated = false;
   }
 
   isAuth(): boolean {
-    return this.user !== undefined;
+    return this.isAuthenticated;
   }
 
   private authSuccessfully(): void {
+    this.isAuthenticated = true;
     this.authChange.next(true);
     this.router.navigate(['/training']).then();
   }
